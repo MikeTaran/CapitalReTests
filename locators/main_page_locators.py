502f0aa60e775685d62c4c7094aafbbc4c126458
MikeTaran <mtaran@ukr.net>
2023-08-27 21:26:58 +0300

    feat: add edge browser, navbar locators

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -7,11 +7,31 @@ class MainPageLocators:
     COUNTRY_MENU = (By.CSS_SELECTOR, 'input[class="fieldDropdown__control fieldDropdown__control--hidden"]')
     INPUT_LIC = (By.CSS_SELECTOR, 'input[class*="js-countriesSearchInput"]')
     LIC_LIST = (By.CSS_SELECTOR, 'ul[class*="js-countriesList"]>li>a')
+    # navbar ProductAndService
+    MENU_PRODUCT = (By.CSS_SELECTOR, 'a[data-type="nav_id2"]')
+    MENU_TRADING_PRODUCTS = (By.CSS_SELECTOR, 'a[data-type="nav_id33"]')
+    MENU_PRODUCT_CFD_TRADING = (By.CSS_SELECTOR, 'a[data-type="nav_id587"]')
+    MENU_PRODUCT_DEMO_ACCOUNT = (By.CSS_SELECTOR, 'a[data-type="nav_id578"]')
+    MENU_PRODUCT_PLATFORM_AND_SERVICE = (By.CSS_SELECTOR, 'a[data-type="nav_id53"]')
+    MENU_PRODUCT_OUR_MOBILE_APPS = (By.CSS_SELECTOR, 'a[data-type="nav_id465"]')
+    MENU_PRODUCT_CAPITAL_API = (By.CSS_SELECTOR, 'a[data-type="nav_id426"]')
+    MENU_PRODUCT_TRADING_VIEW = (By.CSS_SELECTOR, 'a[data-type="nav_id197"]')
+    MENU_PRODUCT_METATRADER4 = (By.CSS_SELECTOR, 'a[data-type="nav_id235"]')
+    MENU_PRODUCT_PROFESSIONAL_TRADERS = (By.CSS_SELECTOR, 'a[]')
+    MENU_PRODUCT_DISCOVER_PRO_TRADING = (By.CSS_SELECTOR, 'a[]')
+    MENU_PRODUCT_CORPORATE_ACCOUNTS = (By.CSS_SELECTOR, 'a[]')
+    MENU_PRODUCT_TRADE_WITH_CAPITAL = (By.CSS_SELECTOR, 'a[]')
+    MENU_PRODUCT_CHARGES_FEES = (By.CSS_SELECTOR, 'a[]')
+    MENU_PRODUCT_CAPITAL_VS_COMPETITOR = (By.CSS_SELECTOR, 'a[]')
+    MENU_PRODUCT_WHY_CAPITAL = (By.CSS_SELECTOR, 'a[]')
+    MENU_PRODUCT_DESKTOP_TRADING = (By.CSS_SELECTOR, 'a[data-type="nav_id77"]')
+    # NAVBAR EDUCATION
+    MENU_PRODUCT_ = (By.CSS_SELECTOR, 'a[]')
+    MENU_PRODUCT_ = (By.CSS_SELECTOR, 'a[]')
     # navbar More
     MENU_MORE = (By.CSS_SELECTOR, 'a[data-type="nav_id16"]')
     MENU_HELP_SUPPORT = (By.CSS_SELECTOR, 'a[data-type="nav_id18"]')
-    MENU_PRODUCT = (By.CSS_SELECTOR, 'a[data-type="nav_id2"]')
-    MENU_PRODUCT_DESKTOP = (By.CSS_SELECTOR, 'a[data-type="nav_id77"]')
+
     MENU_PROF_ACCOUNT = (By.CSS_SELECTOR, 'a[data-type="nav_id89"]')
     MENU_WHY_CAPITAL = (By.CSS_SELECTOR, 'a[data-type="nav_id115"]')
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,16 @@
 import os.path
 import shutil
+import time
 from datetime import datetime
 
 import allure
 from selenium import webdriver
 import pytest
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as OptionsChrome
 from selenium.webdriver.firefox.options import Options as OptionsFirefox
+from selenium.webdriver.edge.options import Options as OptionsEdge
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 
@@ -14,8 +18,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
-headless = True  # режим браузера без отображения (безголовый)
-#headless = False  # режим с отображением браузера
+
 # для корректного отображения кириллицы в параметризаторах
 def pytest_make_parametrize_id(config, val): return repr(val)
 
@@ -26,6 +29,9 @@ def pytest_addoption(parser):
 
     parser.addoption('--language', action='store', default='en',
                      help="Choose language: '--language=en' or '--language=ru'")
+
+    parser.addoption('--headless', action='store_true',
+                     help="Headless mod: '--headless'")
 
 
 # это добавит возможность писать параметры в коммандной строке:
@@ -40,29 +46,40 @@ def pytest_addoption(parser):
 # Запуск браузера(для каждой функции)
 @pytest.fixture(scope="function")  # по умолчанию запускается для каждой функции
 def driver(request):
-    global headless
     browser_name = request.config.getoption(
         "browser_name")  # получаем параметр командной строки browser_name
     user_language = request.config.getoption("language")
+    headless = request.config.getoption("--headless")
+    # headless = True  # режим браузера без отображения (безголовый)
+    # headless = False  # режим с отображением браузера
     driver = None
 
     if browser_name == "chrome":
-        options = Options()
+        chrome_options = OptionsChrome()
         if headless:
-            options.add_argument('--headless')  # Включение режима headless
-        options.add_experimental_option(
+            chrome_options.add_argument('--headless')  # Включение режима headless
+        chrome_options.add_experimental_option(
             'prefs', {'intl.accept_languages': user_language})
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
     elif browser_name == "firefox":
         options_firefox = OptionsFirefox()
-        # options_firefox.add_argument('--headless')  # Включение режима headless
+        if headless:
+            options_firefox.add_argument('--headless')  # Включение режима headless
         options_firefox.set_preference("intl.accept_languages", user_language)
         driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options_firefox)
         # driver = webdriver.Firefox()
+
+    elif browser_name == "edge":
+        options_edge = OptionsEdge()
+        options_edge.page_load_strategy = "eager"  # 'normal'
+        options_edge.add_argument('--language')
+        if headless:
+            options_edge.add_argument('--headless')  # Включение режима headless
+        driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=options_edge)
+
     else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        raise pytest.UsageError("--browser_name should be chrome, firefox or edge")
     yield driver
     attach = driver.get_screenshot_as_png()
     allure.attach(attach, name=f"Screenshot_{datetime.today()}", attachment_type=allure.attachment_type.PNG)
